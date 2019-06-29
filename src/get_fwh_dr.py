dd8fd6fa068582fa45a596c2a182547b3e1fd86e
蒋子航 <32448880+zihangJiang@users.noreply.github.com>
2019-06-30 07:06:05 +0800

    Add files via upload

--- src/get_fwh_dr.py
+++ src/get_fwh_dr.py
@@ -1,4 +1,6 @@
 from get_dr import *
+from get_mesh import *
+from mesh import V2M2
 from measurement import write_align_mesh
 import numpy as np
 import os
@@ -12,28 +14,42 @@ except:
 from tqdm import tqdm
 from multiprocessing.dummy import Pool as ThreadPool
 
+# def generate_mean_face():
+    # print("generating mean face shapes")
+    # V = igl.eigen.MatrixXd()
+    # F = igl.eigen.MatrixXi()
+    # igl.readOBJ('../data/disentangle/Mean_Face.obj', V, F)
+    # try:
+        # os.makedirs('../data/FWH/Mean_Face')
+    # except:
+        # pass
+    
+    # pool = ThreadPool()
+    # for i in tqdm(range(47)):
+        # def process(j):
+            # return om.read_trimesh('../data/FaceWarehouse_Data/Tester_{}/Blendshape/shape_{}.obj'.format(j,i)).points()
+        # gather_mesh = pool.map(process, range(1,151))
+        # mean_mesh = igl.eigen.MatrixXd((sum(gather_mesh)/len(gather_mesh)).astype(np.float64))
+        # igl.writeOBJ('../data/FWH/Mean_Face/shape_{}.obj'.format(i), mean_mesh, F)
+		# write_align_mesh('../data/FWH/Mean_Face/shape_{}.obj'.format(i),
+		# '../data/disentangle/Mean_Face.obj',
+		# '../data/FWH/Mean_Face/shape_{}.obj'.format(i),
+		# index = np.loadtxt('front_part_v.txt',dtype=int))
 def generate_mean_face():
     print("generating mean face shapes")
-    V = igl.eigen.MatrixXd()
-    F = igl.eigen.MatrixXi()
-    igl.readOBJ('../data/disentangle/Mean_Face.obj', V, F)
+    mean_exp = np.load(('../data/{}/MeanFace_data.npy').format('disentangle'))
     try:
         os.makedirs('../data/FWH/Mean_Face')
     except:
         pass
-    
-    pool = ThreadPool()
-    for i in tqdm(range(47)):
-        def process(j):
-            return om.read_trimesh('../data/FaceWarehouse_Data/Tester_{}/Blendshape/shape_{}.obj'.format(j,i)).points()
-        gather_mesh = pool.map(process, range(1,151))
-        mean_mesh = igl.eigen.MatrixXd((sum(gather_mesh)/len(gather_mesh)).astype(np.float64))
-        igl.writeOBJ('../data/FWH/Mean_Face/shape_{}.obj'.format(i), mean_mesh, F)
+    for i in range(47):
+        V2M2(get_mesh('../data/disentangle/Mean_Face.obj', mean_exp[i]), '../data/FWH/Mean_Face/shape_{}.obj'.format(i))
 		write_align_mesh('../data/FWH/Mean_Face/shape_{}.obj'.format(i),
 		'../data/disentangle/Mean_Face.obj',
 		'../data/FWH/Mean_Face/shape_{}.obj'.format(i),
 		index = np.loadtxt('front_part_v.txt',dtype=int))
-		
+	
+	
 def generate_dr_feature():
     print("generating dr feature")
     for j in range(1,151):
